10848b93e0a81c65a740e57cb397b4ba6f3162a4
jinsung <viviamm7@gmail.com>
2026-05-27 13:46:29 +0900

    init : init project

---
+++ src/main/java/com/ddogalmap/DdogalmapApplication.java
@@ -0,0 +1,13 @@
+package com.ddogalmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdogalmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdogalmapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddogalmap/DdogalmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddogalmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdogalmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
