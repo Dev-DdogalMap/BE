191904c2bf63d4afd8d7fc52c83b3121226a04f8
jinsung <viviamm7@gmail.com>
2026-05-29 17:47:29 +0900

    fix : security cors 수정

--- src/main/java/com/ddogalmap/global/security/config/SecurityConfig.java
+++ src/main/java/com/ddogalmap/global/security/config/SecurityConfig.java
@@ -59,7 +59,8 @@ public class SecurityConfig {
 
             config.setAllowedOrigins(List.of(
                     "http://localhost:5173",
-                    "http://localhost:3000"
+                    "http://localhost:3000",
+                    "https://ddogalmap.store"
             ));
             config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
             config.setAllowedHeaders(List.of("*"));
