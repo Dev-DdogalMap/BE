a487967c1087256c3295d4d2a8fa67c5e0af8f96
Eunseong33 <jetaime0105@gmail.com>
2026-06-01 14:51:52 +0900

    feat: 음식점 정보 조회 API 추가 및 filter, interceptor 추가 #14

---
+++ src/main/java/com/ddogalmap/domain/restaurants/dto/projection/RestaurantInfoProjection.java
@@ -0,0 +1,12 @@
+package com.ddogalmap.domain.restaurants.dto.projection;
+
+public interface RestaurantInfoProjection {
+
+		Long getRestaurantId();
+		String getRoadAddressName();
+		String getPhone();
+		String getPlaceUrl();
+		Double getX();
+		Double getY();
+	}
+
