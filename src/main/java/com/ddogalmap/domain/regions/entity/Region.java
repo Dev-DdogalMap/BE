0a68032ad20c1e1471735efd86260bab457db68d
namsujin <a25062401@gmail.com>
2026-06-05 13:53:29 +0900

    feat: 코드리뷰 반영

--- src/main/java/com/ddogalmap/domain/regions/entity/Region.java
+++ src/main/java/com/ddogalmap/domain/regions/entity/Region.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -21,6 +22,7 @@ import java.time.LocalDateTime;
         }
 )
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Region {
 
     @Id
