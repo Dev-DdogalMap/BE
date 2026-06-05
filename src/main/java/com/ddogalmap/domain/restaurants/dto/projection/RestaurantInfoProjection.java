d8846e3486b9cc8efe1711f155fe3b917b5c529d
tjtjdfbf <tjtjdfbf@gmail.co>
2026-06-05 16:14:36 +0900

    chore: 중복 기능 gradle 삭제 'software.amazon.awssdk:s3:2.25.27' main 브랜치와 달랐던 내용들 수정 및 추가

--- src/main/java/com/ddogalmap/domain/restaurants/dto/projection/RestaurantInfoProjection.java
+++ src/main/java/com/ddogalmap/domain/restaurants/dto/projection/RestaurantInfoProjection.java
@@ -3,16 +3,13 @@ package com.ddogalmap.domain.restaurants.dto.projection;
 public interface RestaurantInfoProjection {
 
 	Long getRestaurantId();
-
 	String getPlaceName();
-
 	String getRoadAddressName();
-
 	String getPhone();
-
-	String getPlaceUrl();
-
 	Double getLatitude();
-
 	Double getLongitude();
+	String getFoodType();
+	Integer getDistance();
+	Double getAverageScore();
+	Long getReviewCount();
 }
