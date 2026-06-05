0940199cec29481ebf98af739fdb59434486096a
Lee-Yeonjoo <joos0207@naver.com>
2026-06-05 14:11:49 +0900

    feat: presigned url 발급 api 및 그룹채팅 생성 수정

---
+++ src/main/java/com/ddogalmap/domain/chat/service/ImageUtilService.java
@@ -0,0 +1,53 @@
+package com.ddogalmap.domain.chat.service;
+
+import com.ddogalmap.domain.chat.dto.groupChat.image.UrlDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.time.Duration;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ImageUtilService {
+
+    private final S3Presigner s3Presigner;
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucket;
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    public UrlDto generatePresignedUrl(String folder, String originalFilename) {
+        //파일 이름 형식 예외처리
+        String ext = "";
+        if (originalFilename != null && originalFilename.contains(".")) {
+            ext = originalFilename.substring(originalFilename.lastIndexOf("."));  //확장자
+        }
+        String key = folder + "/" + UUID.randomUUID() + ext;
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        //presigned url 발급
+        PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(
+                PutObjectPresignRequest.builder()
+                        .signatureDuration(Duration.ofMinutes(10))
+                        .putObjectRequest(putObjectRequest)
+                        .build()
+        );
+        return new UrlDto(presignedRequest.url().toString(), key);
+    }
+
+    public String getImageUrl(String key) {
+        return "https://" + bucket + ".s3." + region + ".amazonaws.com/" + key;
+    }
+}
